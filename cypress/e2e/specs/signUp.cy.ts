869372b214465dd64d8067946c6c24337d5f0834
Dalibor Kovář <dalibor.kovar@smartlook.com>
2023-03-29 10:28:49 +0200

    chore: specs update

---
+++ cypress/e2e/specs/signUp.cy.ts
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+// This file will contain tests for the sign up form. Feel free to add as many as you want.
+
+context('Sign up', () => {
+    beforeEach(() => {
+        cy.visit('https://app.alfa.smartlook.cloud/sign/up')
+    })
+
+    // Test sign up form
+    it('Sign up test case', () => {
+        // Fill in some tests
+    })
+
+    // Feel free to modify this file and other files to you liking except the url.
+})
